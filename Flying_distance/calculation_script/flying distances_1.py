04bdcfca05aeb56c21174e7fb33c549878574a67
Yuchen Huang <yuchen520@live.nl>
2020-09-28 23:55:34 +0200

    Added a new project called flying distance with all the modules
    also some file names are changed

---
+++ Flying_distance/calculation_script/flying distances_1.py
@@ -0,0 +1,50 @@
+import math
+
+def convertion (sign, degrees, minutes):
+    if sign == "N" or sign == "W":
+        sign = 1
+    elif sign == "S" or sign == "E":
+        sign = -1
+    else:
+        return "Ops you can only chose between N,W,S,E"
+    # radius convertion (d_r = degrees/radius)
+    r_d = math.pi / 180
+    # minutes convertion
+    m_d = 1/60
+    # final steps
+    m = minutes * m_d
+    convertion = (degrees + m) * r_d * sign
+    return convertion
+
+print (convertion("W",90,0))
+
+
+def haversine (lat1, lat2, lon1, lon2):
+    # haversine splits in 3 parts
+    p1 = math.sin(((lat2-lat1)/2))**2
+    p2 = math.cos(lat1) * math.cos(lat2)
+    p3 = math.sin(((lon2-lon1)/2))**2
+    # calculating a^2 and a
+    a_2 = p1 + p2 * p3
+    a = math.sqrt(a_2)
+    # calculating b^2 and b
+    b_2 = 1 - a_2
+    b = math.sqrt(b_2)
+    # finally the distance
+    distance = 2 * math.atan2(a,b)
+    return distance
+
+def distance (sign_1LA, degrees_1LA, minutes_1LA,
+             sign_1LO, degrees_1LO, minutes_1LO,
+             sign_2LA, degrees_2LA, minutes_2LA,
+             sign_2LO, degrees_2LO, minutes_2LO):
+    lat1 = convertion(sign_1LA, degrees_1LA, minutes_1LA)
+    lon1 = convertion(sign_1LO, degrees_1LO, minutes_1LO)
+    lat2 = convertion(sign_2LA, degrees_2LA, minutes_2LA)
+    lon2 = convertion(sign_2LO, degrees_2LO, minutes_2LO)
+    h = haversine(lat1, lat2, lon1, lon2)
+    dis = h * 6367
+    return dis
+
+
+# print (haversine(1.5707963,0,0,0))
--- Part 3/flying_distances_4.py
+++ Part 3/flying_distances_4.py
@@ -5,9 +5,9 @@ def flying_data():
     city2, lon2, lat2 = ("montreal", (45, 30, "N"), (73, 35, "W"))
     city3, lon3, lat3 = ("auckland", (36, 52, "S"), (174, 45, "E"))
     data_dict = {}
-    data_dict[city1] = lon1, lat1
-    data_dict[city2] = lon2, lat2
-    data_dict[city3] = lon3, lat3
+    data_dict[city1] = city1, lon1, lat1
+    data_dict[city2] = city2, lon2, lat2
+    data_dict[city3] = city3, lon3, lat3
     return data_dict
 
 print(flying_data())
